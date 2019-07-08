a840fd21fc5b80cf9e1b897397341ae12d543f77
ReworldDevs <info@reworld.io>
2019-07-08 10:16:01 -0700

    create an avatar properties script

---
+++ Script Examples/AvatarProperties.lua
@@ -0,0 +1,9 @@
+wait(1) --wait for the avatar to be loaded in game
+
+local avatar = WorkSpace:GetChildByClassName("Avatar") --get the Avatar, a child of WorkSpace 
+--note: avatars are deleted and replaced upon player death. get the avatar again after death to avoid bugs
+
+--examples of some avatar properties that can be modifies
+avatar.MoveSpeed = 0
+avatar.JumpSpeed = 0
+avatar.Health = 0
