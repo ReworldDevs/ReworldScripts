db55f9000cd9b4b84a0e275b369cd068ab93046d
ReworldDevs <info@reworld.io>
2019-07-22 14:19:56 -0700

    Update Particle Effects on vehicle script

--- Script Examples/Flying Vehicle Script/CustomVehicleControls.lua
+++ Script Examples/Flying Vehicle Script/CustomVehicleControls.lua
@@ -22,7 +22,7 @@ end
 function shipControl.Run(delta)
 	local x = math.cos(math.rad(ship.Rotation.y))
 	local y = math.sin(math.rad(ship.Rotation.y))
-	if not w and not s and not e and not q then ship.Velocity = Vector3.New(0,0,0) end
+	if not w and not s and not e and not q then ship.Velocity = Vector3.New(ship.Velocity.x,0,ship.Velocity.z)/1.1 end
 	if not a and not d then ship.RotVelocity = Vector3.New(0,0,0) end
 	if w and not c then ship.Velocity = Vector3.New(10*y,0,10*x) end
 	if w and c then ship.Velocity = Vector3.New(30*y,0,30*x) end
@@ -35,8 +35,18 @@ end
 
 --detect user input begin and change bool variables accordingly
 function shipControl.inputBegin(inputobject) 
-    if tostring(inputobject.InputKeyCode) == "W" and tostring(inputobject.InputState) == "Begin" then w = true end
-    if tostring(inputobject.InputKeyCode) == "C" and tostring(inputobject.InputState) == "Begin" then c = true end
+    if tostring(inputobject.InputKeyCode) == "W" and tostring(inputobject.InputState) == "Begin" then 
+		w = true 
+		ship.Engine1.Enable = true
+		ship.Engine2.Enable = true
+	end
+    if tostring(inputobject.InputKeyCode) == "LeftControl" and tostring(inputobject.InputState) == "Begin" then 
+		c = true 
+		if w then
+			ship.Booster1.Enable = true
+			ship.Booster2.Enable = true
+		end
+	end
 	if tostring(inputobject.InputKeyCode) == "S" and tostring(inputobject.InputState) == "Begin" then s = true end
 	if tostring(inputobject.InputKeyCode) == "E" and tostring(inputobject.InputState) == "Begin" then e = true end
     if tostring(inputobject.InputKeyCode) == "Q" and tostring(inputobject.InputState) == "Begin" then q = true end
@@ -46,8 +56,18 @@ end
 
 --detect user input end and change bool variables accordingly
 function shipControl.inputEnd(inputobject) 
-	if tostring(inputobject.InputKeyCode) == "W" and tostring(inputobject.InputState) == "End" then w = false end
-	if tostring(inputobject.InputKeyCode) == "C" and tostring(inputobject.InputState) == "End" then c = false end
+	if tostring(inputobject.InputKeyCode) == "W" and tostring(inputobject.InputState) == "End" then 
+		w = false 
+		ship.Engine1.Enable = false
+		ship.Engine2.Enable = false
+		ship.Booster1.Enable = false
+		ship.Booster2.Enable = false
+	end
+	if tostring(inputobject.InputKeyCode) == "LeftControl" and tostring(inputobject.InputState) == "End" then 
+		c = false 
+		ship.Booster1.Enable = false
+		ship.Booster2.Enable = false
+	end
 	if tostring(inputobject.InputKeyCode) == "S" and tostring(inputobject.InputState) == "End" then s = false end
 	if tostring(inputobject.InputKeyCode) == "E" and tostring(inputobject.InputState) == "End" then e = false end
     if tostring(inputobject.InputKeyCode) == "Q" and tostring(inputobject.InputState) == "End" then q = false end
